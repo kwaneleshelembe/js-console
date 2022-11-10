185e07544279882b22fedf7d9d4f699340202b11
kwanele <kwaneleshelembe841@gmail.com>
2022-11-10 11:38:56 +0200

    added theme functionality

--- js/script.js
+++ js/script.js
@@ -52,3 +52,31 @@ const consoleBtn = document.querySelector("#console-btn");
 consoleBtn.addEventListener("click", (e) => {
     openConsole();
 })
+
+const themeContainer=document.querySelector("#theme");
+
+let light=true;
+
+function darkMode(){
+    document.body.className="bg-dark text-light";
+    consoleBtn.className="btn btn-primary col-4 col-lg-3 offset-1 offset-lg-2";
+    runBtn.className="btn btn-success col-4 col-lg-3 offset-2";
+    themeContainer.innerHTML="<i class='fa fa-sun fa-lg' title='light mode'></i>"
+}
+
+function lightMode(){
+    document.body.className="bg-light text-dark";
+    consoleBtn.className="btn btn-outline-primary col-4 col-lg-3 offset-1 offset-lg-2";
+    runBtn.className="btn btn-outline-success col-4 col-lg-3 offset-2";
+    themeContainer.innerHTML="<i class='fa fa-moon fa-lg' title='dark mode'></i>"
+}
+
+themeContainer.addEventListener("click",(e)=>{
+    if(light){
+        darkMode();
+        light=false;
+    }else{
+        lightMode();
+        light=true;
+    }
+});
